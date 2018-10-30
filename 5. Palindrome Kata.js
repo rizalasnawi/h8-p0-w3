fd0980f95e021a20f2da0af872f81561a282c134
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2018-10-30 22:24:10 +0700

    Palindrom

---
+++ 5. Palindrome Kata.js
@@ -0,0 +1,19 @@
+function palindrome(kata) {
+  // you can only write your code here!
+  balik="";
+  for(var i=kata.length-1; i>=0; i--){
+      balik+=kata[i];
+    }
+    
+    if(balik===kata){
+        return true;
+    }else{
+        return false;      
+    }
+}
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
