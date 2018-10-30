0ba555a0767237753360658b1eb13464a1891550
rizalasnawi <44157537+rizalasnawi@users.noreply.github.com>
2018-10-30 21:56:38 +0700

    Add files via upload

---
+++ 3. Melakukan Looping Data Array.js
@@ -0,0 +1,18 @@
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
+
+function dataHandling(input){
+    var output="";
+
+    for(var i=0; i<=input.length-1;i++){
+        output+= "Nomor ID: "+ input[i][0] +"\n" +"Nama Lengkap:  "+ input[i][1] +"\n"+ "TTL: "+ input [i][2]+ " "+input[i][3] + "\n" +"Hobi: "+input[i][4]+"\n"+"\n";
+    }
+    
+   return output;
+}
+
+console.log(dataHandling(input));
